56ba24d08141df7f5fc9b608b2238672f14efbf3
galen086411 <110959188+galen086411@users.noreply.github.com>
2023-05-02 12:39:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,12 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.242/g' package/base-files/files/bin/config_generate
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Router'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /Kinoko-Galen build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+ 
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+
