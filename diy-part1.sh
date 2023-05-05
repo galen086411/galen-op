12fa678c6c5ac4e4ef2dae555ae547aea7d992a3
galen086411 <110959188+galen086411@users.noreply.github.com>
2023-05-05 16:22:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,13 +12,13 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # 添加软件源（打开下方仓库链接可以查看包含的插件列表，一般选前两个源就行）
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
-# echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
+echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
